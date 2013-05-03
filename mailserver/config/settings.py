e1e9d6ecbe128b5457a93f02cdd6cd7792b4e356
Tim Watts <tim@readevalprint.com>
2013-05-03 04:00:34 +0000

    removed mailpipe.io specific code

--- mailserver/config/settings.py
+++ mailserver/config/settings.py
@@ -1,6 +1,9 @@
-
 relay_config = {'host': 'localhost', 'port': 8825}
 receiver_config = {'host': '0.0.0.0', 'port': 25}
 handlers = ['app.handlers.process']
-router_defaults = {'host': 'pipe1.mailpipe.io'}
 template_config = {'dir': 'app', 'module': 'templates'}
+router_defaults = {}
+try:
+    from local_settings import *
+except Exception as e:
+    print "Coulding load local_settings:", e
