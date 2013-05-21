c92b300cb411245f3249d84477cf017384b4ad63
Tim Watts <tim@radevalprint.com>
2013-05-21 18:01:31 +0000

    Added email_url query string when calling the calback url

--- mailpipe/mailpipe/tasks.py
+++ mailpipe/mailpipe/tasks.py
@@ -1,5 +1,7 @@
 import celery
 import requests
+from django.contrib.sites.models import Site
+from django.core.urlresolvers import reverse
 
 
 @celery.task(name='mailpipe.tasks.process_email')
@@ -20,11 +22,14 @@ def notify_callback(email_id, timeout=1):
     email = email[0]
     retry = True
     try:
-        r = requests.get(email.account.callback_url)
+        domain = Site.objects.get_current().domain
+        path = reverse('email-detail', kwargs={'pk': email.pk})
+        r = requests.get(email.account.callback_url,
+                         params={'email_url': domain + path})
         if r.status_code == 200:
             retry = False
-    except:
-        pass
+    except Exception as e:
+        print e
     if retry and timeout < 60 * 60: # TODO: Make setting
         notify_callback.apply_async([email_id], {'timeout': timeout*2}, countdown=timeout)
     return not retry
